52a46232fbba6b4c41dc43b18d194d754abfb3e4
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-22 16:24:22 +0200

    modifiche

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-var card = {
+const card = {
   id: 1,
   name: "Bloodfire Colossus",
   launchCost: ["6", "R", "R"],
@@ -38,3 +38,37 @@ var card = {
   cardImage: "../img/pic.jpg",
 };
 console.table(card);
+
+// ________________________STAMPA IN HTML__________________________
+
+const cardSection = document.getElementById("cards");
+
+let cardTemplate = `
+<ul class="card";
+<li><strong>id:</strong> ${card.id}</li>
+<li><strong>Nome:</strong> ${card.name}</li>
+<li><strong>Costo di lancio:</strong> ${card.launchCost.join(" - ")}</li>
+<li><strong>Costi mana combinato:</strong> ${card.costoManaCombinato}</li>
+<li><strong>Tipo di carta:</strong> ${card.cardType} - ${card.subType}</li>
+</ul>
+`;
+
+cardSection.innerHTML = cardTemplate;
+
+let subType;
+if (card.subType) {
+  subType = card.subType;
+} else {
+  subType = " ";
+}
+
+// function absenceData(date) {
+//   var date;
+//   if (date) {
+//     date;
+//   } else {
+//     date = " ";
+//   }
+//   return date;
+// }
+// var subType = absenceData(card.subType);
