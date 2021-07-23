7d7db236ec05a995b457ee58b599318d1856fa85
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-23 16:27:21 +0200

    Funzione di preparazione template base

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-const card = {
+const firstCard = {
   id: 1,
   name: "Bloodfire Colossus",
   launchCost: ["6", "R", "R"],
@@ -37,104 +37,110 @@ const card = {
     cardImage: "img/Image.jpg",
   },
 };
-console.table(card);
+console.table(firstCard);
 
 // ________________________STAMPA IN HTML__________________________
 
 const cardSection = document.getElementById("cards");
 
-// ________________________PER ABILITA'___________________________________
-
-let abilitiesContent = "<em>Nessuna abilità</em>";
-if (card.abilities.length) {
-  abilitiesContent = "<ul>";
-  for (let i = 0; i < card.abilities.length; i++) {
-    const currentAbility = card.abilities[i];
-    abilitiesContent += `<li> Costo di lancio: ' ${absenceData(
-      currentAbility.launchCost.join(", ")
-    )}'</li>`;
-    abilitiesContent += `<li> Descrizione: ' ${absenceData(
-      currentAbility.description
-    )}'</li>`;
-  }
-  abilitiesContent += "</ul>";
-}
-// _______________________________________________________________________
-
-let cardTemplate = `
-<ul class="card";
-  <li><strong>id:</strong>
-      ${absenceData(card.id)}
-  </li>
-  <li><strong>Nome:</strong>
-      ${absenceData(card.name)}
-  </li>
-  <li><strong>Costo di lancio:</strong>
-      ${absenceData(card.launchCost.join(" - "))}
-  </li>
-  <li><strong>Costi mana combinato:</strong>
-      ${absenceData(card.costoManaCombinato)}
-  </li>
-  <li><strong>Tipo di carta:</strong> 
-      ${absenceData(card.cardType)} 
-      - ${absenceData(card.subType)}
-  </li>
-  <li><strong>Espansione:</strong>
-     <ul>
-        <li><strong>Ristampa:</strong>
-          ${absenceData(card.expansion.id)}
-        </li>
-        <li><strong>Nome:</strong>
-          ${absenceData(card.expansion.name)}
-        </li>
-        <li><strong>Rarità:</strong>
-          ${absenceData(card.expansion.rarity)}
-        </li>
-         <li><strong>Numero Collezione:</strong>
-          ${absenceData(card.expansion.collettionNr)}
-          /${absenceData(card.expansion.totalCard)}
-        </li>
-     </ul>
-  </li>
-  <li><strong>Testo di colore:</strong> 
-      ${absenceData(card.flavorTex.quote)} 
-      - ${absenceData(card.flavorTex.author)}
-  </li>
-  <li><strong>Abilità:</strong> 
-      ${absenceData(abilitiesContent)} 
-  </li>
-  <li><strong>Costituzione</strong>
-      ${absenceData(card.constitution)}
-  </li>
-  <li><strong>Forza</strong>
-      ${absenceData(card.strength)}
-  </li>
-  <li><strong>Colore Bordo</strong>
-      ${absenceData(card.borderColor)}
-  </li>
-  <li><strong>Illustrazione</strong>
-      <ul>
-      <li><strong>immagine : </strong> <img src="${absenceData(
-        card.illustrator.cardImage
-      )}" /></li>
-        <li><strong>Autore:</strong>
-          ${absenceData(
-            card.illustrator.author.name
-          )} - <strong>id:</strong> ${absenceData(card.illustrator.author.id)}
-        </li>
-     </ul>
-  </li>
-</ul>
-`;
+const cardTemplate = createCardTemplate(firstCard);
 
 cardSection.innerHTML = cardTemplate;
 
-// ________________________FUNZIONI__________________________
+function createCardTemplate(card) {
+  // ________________________PER ABILITA'___________________________________
 
-function absenceData(date) {
-  if (date) {
-    return date;
-  } else {
-    return " ";
+  let abilitiesContent = "<em>Nessuna abilità</em>";
+  if (card.abilities.length) {
+    abilitiesContent = "<ul>";
+    for (let i = 0; i < card.abilities.length; i++) {
+      const currentAbility = card.abilities[i];
+      abilitiesContent += `<li> Costo di lancio: ' ${absenceData(
+        currentAbility.launchCost.join(", ")
+      )}'</li>`;
+      abilitiesContent += `<li> Descrizione: ' ${absenceData(
+        currentAbility.description
+      )}'</li>`;
+    }
+    abilitiesContent += "</ul>";
   }
+  // _______________________________________________________________________
+
+  let cardTemplate = `
+  <ul class="card";
+    <li><strong>id:</strong>
+        ${absenceData(card.id)}
+    </li>
+    <li><strong>Nome:</strong>
+        ${absenceData(card.name)}
+    </li>
+    <li><strong>Costo di lancio:</strong>
+        ${absenceData(card.launchCost.join(" - "))}
+    </li>
+    <li><strong>Costi mana combinato:</strong>
+        ${absenceData(card.costoManaCombinato)}
+    </li>
+    <li><strong>Tipo di carta:</strong> 
+        ${absenceData(card.cardType)} 
+        - ${absenceData(card.subType)}
+    </li>
+    <li><strong>Espansione:</strong>
+       <ul>
+          <li><strong>Ristampa:</strong>
+            ${absenceData(card.expansion.id)}
+          </li>
+          <li><strong>Nome:</strong>
+            ${absenceData(card.expansion.name)}
+          </li>
+          <li><strong>Rarità:</strong>
+            ${absenceData(card.expansion.rarity)}
+          </li>
+           <li><strong>Numero Collezione:</strong>
+            ${absenceData(card.expansion.collettionNr)}
+            /${absenceData(card.expansion.totalCard)}
+          </li>
+       </ul>
+    </li>
+    <li><strong>Testo di colore:</strong> 
+        ${absenceData(card.flavorTex.quote)} 
+        - ${absenceData(card.flavorTex.author)}
+    </li>
+    <li><strong>Abilità:</strong> 
+        ${absenceData(abilitiesContent)} 
+    </li>
+    <li><strong>Costituzione</strong>
+        ${absenceData(card.constitution)}
+    </li>
+    <li><strong>Forza</strong>
+        ${absenceData(card.strength)}
+    </li>
+    <li><strong>Colore Bordo</strong>
+        ${absenceData(card.borderColor)}
+    </li>
+    <li><strong>Illustrazione</strong>
+        <ul>
+        <li><strong>immagine : </strong> <img src="${absenceData(
+          card.illustrator.cardImage
+        )}" /></li>
+          <li><strong>Autore:</strong>
+            ${absenceData(
+              card.illustrator.author.name
+            )} - <strong>id:</strong> ${absenceData(card.illustrator.author.id)}
+          </li>
+       </ul>
+    </li>
+  </ul>
+  `;
+
+  // ________________________FUNZIONI__________________________
+
+  function absenceData(date) {
+    if (date) {
+      return date;
+    } else {
+      return " ";
+    }
+  }
+
+  return cardTemplate;
 }
