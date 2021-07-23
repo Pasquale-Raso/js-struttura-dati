c17e6c0553c1a81428e9a99017bafd854d1e9016
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-23 19:01:30 +0200

    creo mazzo di carte e stampo in html

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,134 @@
-const firstCard = {
-  id: 1,
-  name: "Bloodfire Colossus",
-  launchCost: ["6", "R", "R"],
-  costoManaCombinato: 8,
-  cardType: "Creatura",
-  subType: "Giant",
-  expansion: {
-    id: 9,
-    name: "Espansione",
-    rarity: "golden",
-    collettionNr: 12,
-    totalCard: 43,
-  },
-  flavorTex: {
-    quote: "We...",
-    author: "Orso Bubu",
-  },
-  abilities: [
-    {
-      launchCost: ["R", "T"],
-      description: "lorem...",
+const myDeck = [
+  {
+    id: 1,
+    name: "Bloodfire Colossus",
+    launchCost: ["6", "R", "R"],
+    costoManaCombinato: 8,
+    cardType: "Creatura",
+    subType: "Giant",
+    expansion: {
+      id: 9,
+      name: "Espansione",
+      rarity: "golden",
+      collettionNr: 12,
+      totalCard: 43,
     },
-    {
-      launchCost: ["W", "B", "T"],
-      description: "lorem...",
+    flavorTex: {
+      quote: "We...",
+      author: "Orso Bubu",
     },
-  ],
-  constitution: 13,
-  strength: 13,
-  borderColor: "#000",
-  illustrator: {
-    author: {
-      id: 1,
-      name: "Greg Smith",
+    abilities: [
+      {
+        launchCost: ["R", "T"],
+        description: "lorem...",
+      },
+      {
+        launchCost: ["W", "B", "T"],
+        description: "lorem...",
+      },
+    ],
+    constitution: 13,
+    strength: 13,
+    borderColor: "#000",
+    illustrator: {
+      author: {
+        id: 1,
+        name: "Greg Smith",
+      },
+      cardImage: "img/Image.jpg",
     },
-    cardImage: "img/Image.jpg",
   },
+
+  // _____________________________________________________________
+
+  {
+    id: 2,
+    name: "Krosan Cloudscraper",
+    launchCost: ["7", "G", "G"],
+    costoManaCombinato: 10,
+    cardType: "Creatura",
+    subType: "Beast Mutant",
+    expansion: {
+      id: 9,
+      name: "Espansione",
+      rarity: "Special",
+      collettionNr: 13,
+      totalCard: 13,
+    },
+    flavorTex: {
+      quote: "We...",
+      author: "Orso Bubu",
+    },
+    abilities: [
+      {
+        launchCost: ["R", "T"],
+        description: "lorem...",
+      },
+      {
+        launchCost: ["W", "B", "T"],
+        description: "lorem...",
+      },
+    ],
+    constitution: 13,
+    strength: 13,
+    borderColor: "#000",
+    illustrator: {
+      author: {
+        id: 2,
+        name: "Ron Spears",
+      },
+      cardImage: "img/Image2.jpg",
+    },
+  },
+  // _____________________________________________________________
+  {
+    id: 3,
+    name: "Troll Ascetic",
+    launchCost: ["1", "G", "G"],
+    costoManaCombinato: 3,
+    cardType: "Creatura",
+    subType: "Troll Shaman",
+    expansion: {
+      id: 9,
+      name: "Espansione",
+      rarity: "rare",
+      collettionNr: 3,
+      totalCard: 2,
+    },
+    flavorTex: {
+      quote: "We...",
+      author: "Orso Bubu",
+    },
+    abilities: [],
+    constitution: 13,
+    strength: 13,
+    borderColor: "#000",
+    illustrator: {
+      author: {
+        id: 2,
+        name: "Puddnhead",
+      },
+      cardImage: "img/Image3.jpg",
+    },
+  },
+];
+
+//!________________________STAMPA IN HTML(FUNCTION JS6)__________________________
+
+const renderDeck = (deck) => {
+  const cardSection = document.getElementById("cards");
+
+  let deckTemplate = "";
+  for (let i = 0; i < deck.length; i++) {
+    const currentCard = deck[i];
+    deckTemplate += createCardTemplate(currentCard);
+  }
+
+  cardSection.innerHTML = deckTemplate;
 };
-console.table(firstCard);
+renderDeck(myDeck);
 
-// ________________________STAMPA IN HTML__________________________
-
-const cardSection = document.getElementById("cards");
-
-const cardTemplate = createCardTemplate(firstCard);
-
-cardSection.innerHTML = cardTemplate;
+//! ________________________TEMPLATE (FUNCTION JS3)__________________________
 
 function createCardTemplate(card) {
   // ________________________PER ABILITA'___________________________________
@@ -67,72 +150,75 @@ function createCardTemplate(card) {
   // _______________________________________________________________________
 
   let cardTemplate = `
-  <ul class="card";
-    <li><strong>id:</strong>
-        ${absenceData(card.id)}
-    </li>
-    <li><strong>Nome:</strong>
-        ${absenceData(card.name)}
-    </li>
-    <li><strong>Costo di lancio:</strong>
-        ${absenceData(card.launchCost.join(" - "))}
-    </li>
-    <li><strong>Costi mana combinato:</strong>
-        ${absenceData(card.costoManaCombinato)}
-    </li>
-    <li><strong>Tipo di carta:</strong> 
-        ${absenceData(card.cardType)} 
-        - ${absenceData(card.subType)}
-    </li>
-    <li><strong>Espansione:</strong>
-       <ul>
-          <li><strong>Ristampa:</strong>
-            ${absenceData(card.expansion.id)}
-          </li>
-          <li><strong>Nome:</strong>
-            ${absenceData(card.expansion.name)}
-          </li>
-          <li><strong>Rarità:</strong>
-            ${absenceData(card.expansion.rarity)}
-          </li>
-           <li><strong>Numero Collezione:</strong>
-            ${absenceData(card.expansion.collettionNr)}
-            /${absenceData(card.expansion.totalCard)}
-          </li>
-       </ul>
-    </li>
-    <li><strong>Testo di colore:</strong> 
-        ${absenceData(card.flavorTex.quote)} 
-        - ${absenceData(card.flavorTex.author)}
-    </li>
-    <li><strong>Abilità:</strong> 
-        ${absenceData(abilitiesContent)} 
-    </li>
-    <li><strong>Costituzione</strong>
-        ${absenceData(card.constitution)}
-    </li>
-    <li><strong>Forza</strong>
-        ${absenceData(card.strength)}
-    </li>
-    <li><strong>Colore Bordo</strong>
-        ${absenceData(card.borderColor)}
-    </li>
-    <li><strong>Illustrazione</strong>
-        <ul>
-        <li><strong>immagine : </strong> <img src="${absenceData(
-          card.illustrator.cardImage
-        )}" /></li>
-          <li><strong>Autore:</strong>
-            ${absenceData(
-              card.illustrator.author.name
-            )} - <strong>id:</strong> ${absenceData(card.illustrator.author.id)}
-          </li>
-       </ul>
-    </li>
-  </ul>
+  <div class="card">
+      <ul>
+        <li><strong>id:</strong>
+            ${absenceData(card.id)}
+        </li>
+        <li><strong>Nome:</strong>
+            ${absenceData(card.name)}
+        </li>
+        <li><strong>Costo di lancio:</strong>
+            ${absenceData(card.launchCost.join(" - "))}
+        </li>
+        <li><strong>Costi mana combinato:</strong>
+            ${absenceData(card.costoManaCombinato)}
+        </li>
+        <li><strong>Tipo di carta:</strong> 
+            ${absenceData(card.cardType)} 
+            - ${absenceData(card.subType)}
+        </li>
+        <li><strong>Espansione:</strong>
+          <ul>
+              <li><strong>Ristampa:</strong>
+                ${absenceData(card.expansion.id)}
+              </li>
+              <li><strong>Nome:</strong>
+                ${absenceData(card.expansion.name)}
+              </li>
+              <li><strong>Rarità:</strong>
+                ${absenceData(card.expansion.rarity)}
+              </li>
+              <li><strong>Numero Collezione:</strong>
+                ${absenceData(card.expansion.collettionNr)}
+                /${absenceData(card.expansion.totalCard)}
+              </li>
+          </ul>
+        </li>
+        <li><strong>Testo di colore:</strong> 
+            ${absenceData(card.flavorTex.quote)} 
+            - ${absenceData(card.flavorTex.author)}
+        </li>
+        <li><strong>Abilità:</strong> 
+            ${absenceData(abilitiesContent)} 
+        </li>
+        <li><strong>Costituzione</strong>
+            ${absenceData(card.constitution)}
+        </li>
+        <li><strong>Forza</strong>
+            ${absenceData(card.strength)}
+        </li>
+        <li><strong>Colore Bordo</strong>
+            ${absenceData(card.borderColor)}
+        </li>
+        <li><strong>Illustrazione</strong>
+            <ul>
+            <li><strong>immagine : </strong>  <img class="img" src="${absenceData(
+              card.illustrator.cardImage
+            )}" /></li>
+              <li><strong>Autore:</strong>
+                ${absenceData(
+                  card.illustrator.author.name
+                )} - <strong>id:</strong> 
+                  ${absenceData(card.illustrator.author.id)}
+              </li>
+          </ul>
+        </li>
+      </ul>
+  </div>  
   `;
 
-  // ________________________FUNZIONI__________________________
+  // ________________________FUNZIONE ASSENZA DATO__________________________
 
   function absenceData(date) {
     if (date) {
